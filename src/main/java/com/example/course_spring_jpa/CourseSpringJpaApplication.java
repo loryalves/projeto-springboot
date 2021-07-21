6917e9813747f87bd4112d33628f3d16d361922d
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-21 19:26:43 -0300

    Projeto criado

---
+++ src/main/java/com/example/course_spring_jpa/CourseSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_spring_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpringJpaApplication.class, args);
+    }
+
+}

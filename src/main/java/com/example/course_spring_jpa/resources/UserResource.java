bb306b7bf581ba92c1edfde80335859871b4ccdf
Lorrane Alves <Lorrane.dev@gmail.com>
2021-07-22 16:21:10 -0300

    Teste inicial com resource e entidade User

---
+++ src/main/java/com/example/course_spring_jpa/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.course_spring_jpa.resources;
+
+import com.example.course_spring_jpa.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+}
